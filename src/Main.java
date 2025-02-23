9dd1bbc810e13e62b7125b64e45818d02a03ac6b
lauramelissa661 <Lauramelissa661@gmail.com>
2025-02-23 17:25:59 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Vistula");
+
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
